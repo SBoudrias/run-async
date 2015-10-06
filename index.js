a4bc2d29fc48c5d870cc5637b83df0e15dad2ba3
James Talmage <james@talmage.io>
2015-10-06 01:56:41 -0400

    support promises / switch to node style callbacks

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var once = require('once');
+var isPromise = require('is-promise');
 
 /**
  * Run a function asynchronously or synchronously
@@ -12,14 +13,24 @@ var once = require('once');
 
 module.exports = function (func, cb) {
   var async = false;
-  var answer = func.apply({
-    async: function () {
-      async = true;
-      return once(cb);
-    }
-  }, Array.prototype.slice.call(arguments, 2) );
+  var cb = once(cb);
 
-  if (!async) {
-    setImmediate(cb.bind(null, answer));
+  try {
+    var answer = func.apply({
+      async: function () {
+        async = true;
+        return cb;
+      }
+    }, Array.prototype.slice.call(arguments, 2) );
+
+    if (!async) {
+      if (isPromise(answer)) {
+        answer.then(cb.bind(null, null), cb);
+      } else {
+        setImmediate(cb.bind(null, null, answer));
+      }
+    }
+  } catch (e) {
+    setImmediate(cb.bind(null, e));
   }
 };
