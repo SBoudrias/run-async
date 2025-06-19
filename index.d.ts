472cc6d29fe63ef23ca9f966180d4a13d58f2284
Simon Boudrias <admin@simonboudrias.com>
2025-06-19 15:50:52 -0400

    Fix: trying again to publish types

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,5 @@
-export default function runAsync<F extends (...args: any[]) => any>(
+declare function runAsync<F extends (...args: any[]) => any>(
   func: F,
 ): (...args: Parameters<F>) => Promise<Awaited<ReturnType<F>>>;
+
+export = runAsync;
